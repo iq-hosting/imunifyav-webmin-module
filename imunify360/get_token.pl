a2abce1f799d887c39d301937da43b838af7b45b
iq-hosting <41027928+iq-hosting@users.noreply.github.com>
2024-12-21 20:52:39 +0300

    Update get_token.pl
    Improved token handling

--- imunify360/get_token.pl
+++ imunify360/get_token.pl
@@ -5,17 +5,20 @@ use warnings;
 sub get_token {
     my $token_file = "imunifytokens.txt";
     system("imunify360-agent login get --username root > $token_file");
-    chmod 0600, $token_file or die "Could not set permissions on $token_file: $!";
+    chmod 0600, $token_file;
     open(my $fh, '<', $token_file) or die "Could not open token file: $!";
     my $token = <$fh>;
     close($fh);
     chomp $token;
-    sleep 1;
-    if (unlink $token_file) {
-        log_change("Token file $token_file deleted successfully.");
+
+    if (open(my $empty_fh, '>', $token_file)) {
+        print $empty_fh "";
+        close($empty_fh);
     } else {
-        log_change("Failed to delete token file $token_file.");
+        log_change("Failed to empty token file $token_file.");
     }
+
     return $token;
 }
+
 1;
